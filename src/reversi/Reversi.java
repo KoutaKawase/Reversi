86c058867498356c1d279281bdd98d1d4e7e833f
KoutaKawase <suhrkawase@gmail.com>
2019-12-21 11:29:15 +0900

    🚧 コメント追加

--- src/reversi/Reversi.java
+++ src/reversi/Reversi.java
@@ -2,6 +2,6 @@ package reversi;
 
 public class Reversi {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        // 2人のプレイヤーがそれぞれ黒番と白番のどちらを担当するかを決める
     }
 }
