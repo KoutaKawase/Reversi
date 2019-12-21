a5a63f23b98c57fed1b25de49834215e8c33dbc4
KoutaKawase <suhrkawase@gmail.com>
2019-12-21 11:43:54 +0900

    🚧 二人のプレイヤーを作成できるようにした

--- src/reversi/Reversi.java
+++ src/reversi/Reversi.java
@@ -3,5 +3,10 @@ package reversi;
 public class Reversi {
     public static void main(String[] args) throws Exception {
         // 2人のプレイヤーがそれぞれ黒番と白番のどちらを担当するかを決める
+        Player playerBlack = new Player(PieceColor.BLACK_PIECE);
+        Player playerWhite = new Player(PieceColor.WHITE_PIECE);
+
+        System.out.println(playerBlack);
+        System.out.println(playerWhite);
     }
 }
