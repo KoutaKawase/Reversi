84a849b9cc1a49f446e6e6e026a1ae41e2e93283
KoutaKawase <suhrkawase@gmail.com>
2019-12-21 11:39:36 +0900

    🚧 PlayerにpieceColorメンバ追加

--- src/reversi/Player.java
+++ src/reversi/Player.java
@@ -4,5 +4,9 @@ package reversi;
  * Player プレイヤーに関するデータあつかう役目
  */
 public class Player {
+    private PieceColor pieceColor;
 
+    public Player(PieceColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
 }
