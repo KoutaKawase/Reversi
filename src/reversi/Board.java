451fd445d61b146b46b8d70e2b245ac0e0b8ed51
KoutaKawase <suhrkawase@gmail.com>
2019-12-21 12:07:51 +0900

    🚧 Boardクラス作成

---
+++ src/reversi/Board.java
@@ -0,0 +1,8 @@
+package reversi;
+
+/**
+ * Board ボードの状態を管理する役
+ */
+public class Board {
+
+}
---
+++ src/reversi/Piece.java
@@ -0,0 +1,6 @@
+package reversi;
+
+//NONEは何も置かれていない状態を指す
+public enum Piece {
+    BLACK_PIECE, WHITE_PIECE, NONE
+}
