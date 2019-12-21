d81f38cc8511c06e8695b8b8cad6dff42b1b3b04
KoutaKawase <suhrkawase@gmail.com>
2019-12-21 12:51:52 +0900

    🚧 Boardステートを文字列出力できた！

--- src/reversi/Board.java
+++ src/reversi/Board.java
@@ -4,7 +4,52 @@ package reversi;
  * Board ボードの状態を管理する役
  */
 public class Board {
-    public static final int MAX_BOARD_COLUMN = 8;
-    public static final int MAX_BOARD_ROW = 8;
+    public static final int MAX_BOARD_COLUMN = 6;
+    public static final int MAX_BOARD_ROW = 6;
     private Piece[][] boardState = new Piece[MAX_BOARD_COLUMN][MAX_BOARD_ROW];
+
+    public String getBoardState() {
+        return this.toString();
+    }
+
+    @Override
+    public String toString() {
+        String board = "";
+
+        for (int column = 0; column < MAX_BOARD_COLUMN; column++) {
+            for (int row = 0; row < MAX_BOARD_ROW; row++) {
+                switch (this.boardState[column][row]) {
+                case BLACK_PIECE:
+                    board += "■ ";
+                    break;
+                case WHITE_PIECE:
+                    board += "□ ";
+                    break;
+                case NONE:
+                    board += "# ";
+                    break;
+                }
+            }
+            board += "\n";
+        }
+        return board;
+    }
+
+    public void initializeBoard() {
+        for (int column = 0; column < MAX_BOARD_COLUMN; column++) {
+            for (int row = 0; row < MAX_BOARD_ROW; row++) {
+                if ((column == 2 && row == 2) || (column == 3 && row == 3)) {
+                    boardState[column][row] = Piece.WHITE_PIECE;
+                    continue;
+                }
+
+                if ((column == 3 && row == 2) || (column == 2 && row == 3)) {
+                    boardState[column][row] = Piece.BLACK_PIECE;
+                    continue;
+                }
+
+                boardState[column][row] = Piece.NONE;
+            }
+        }
+    }
 }
--- src/reversi/Reversi.java
+++ src/reversi/Reversi.java
@@ -6,6 +6,8 @@ public class Reversi {
         Player playerBlack = new Player(Piece.BLACK_PIECE);
         Player playerWhite = new Player(Piece.WHITE_PIECE);
         // 盤面中央の4マスに黒と白の石を2つずつ置く。右上と左下が黒、左上と右下が白になるように互い違いに配置する。
-
+        Board board = new Board();
+        board.initializeBoard();
+        System.out.println(board.getBoardState());
     }
 }
