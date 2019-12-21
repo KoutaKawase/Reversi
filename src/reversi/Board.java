6d99076baaaed8bfe3c61fbf7835eeee748bf4f4
KoutaKawase <suhrkawase@gmail.com>
2019-12-21 12:25:24 +0900

    🚧 Boardにステートプロパティ追加

--- src/reversi/Board.java
+++ src/reversi/Board.java
@@ -4,5 +4,7 @@ package reversi;
  * Board ボードの状態を管理する役
  */
 public class Board {
-
+    public static final int MAX_BOARD_COLUMN = 8;
+    public static final int MAX_BOARD_ROW = 8;
+    private Piece[][] boardState = new Piece[MAX_BOARD_COLUMN][MAX_BOARD_ROW];
 }
